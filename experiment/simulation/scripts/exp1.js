bb7827c68624d3988452e67be6678e1eff102c45
Varun Pal Singh <singhvarun928@gmail.com>
2023-07-01 16:37:01 +0530

    completed starter page

--- experiment/simulation/scripts/exp1.js
+++ experiment/simulation/scripts/exp1.js
@@ -1,13 +1,37 @@
-function starter(){
-    document.querySelector("#mainStep").style.visibility = "hidden";
-    document.querySelector("#start-btn").style.visibility = "hidden";
+// THIS SINGLE FUNCTION WILL STOP THE setTimeOut Functions
 
-    document.querySelector("#starter").style.visibility = "visible";
+function myStopFuntion(){
+    clearTimeout(myTimeout);
 }
 
-function move(){
-    document.querySelector("#starter-drawingBoard").style.visibility = "hidden";
-    document.querySelector("#starter-drawingSheet").style.visibility = "hidden";
+// STEP 1
 
-    document.querySelector("#starter-gif").style.visibility = "visble";
+function step1(){
+    document.querySelector("#mainStep").style.visibility = "hidden";
+    document.querySelector("#png1-mainStep").style.visibility = "hidden";
+    document.querySelector("#next-to-step1").style.visibility = "hidden";
+
+    document.querySelector("#step1").style.visibility = "visible";
+    document.querySelector("#png1-step1").style.visibility = "visible";
+    document.querySelector("#png2-step1").style.visibility = "visible";
+    
+    myTimeout = setTimeout(myGreeting1Step1, 1000);
+}
+ 
+function moveSheet(){
+    document.querySelector("#png1-step1").style.visibility = "hidden";
+    document.querySelector("#png2-step1").style.visibility = "hidden";
+    document.querySelector("#arr1-step1").style.visibility = "hidden";
+    document.querySelector("#trans1-step1").style.visibility = "hidden";
+
+    document.querySelector("#gif1-step1").style.visibility = "visible";
+}
+
+// TIME DELAY FUNCTION OF STEP 1
+
+function myGreeting1Step1(){
+    document.querySelector("#arr1-step1").style.visibility = "visible";
+    document.querySelector("#trans1-step1").style.visibility = "visible";
+
+    myStopFuntion();
 }
