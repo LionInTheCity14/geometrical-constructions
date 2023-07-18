5ed244f0c7d41a602a9a41fa387eff213ec31a8a
Varun Pal Singh <singhvarun928@gmail.com>
2023-07-18 11:09:28 +0530

    created new exp1 file

--- experiment/simulation/scripts/exp1.js
+++ experiment/simulation/scripts/exp1.js
@@ -4,34 +4,22 @@ function myStopFuntion(){
     clearTimeout(myTimeout);
 }
 
-// STEP 1
+function step1MoveSheet1(){
+    document.querySelector("#step1-png1").style.visibility = "hidden";
+    document.querySelector("#step1-trans1").style.visibility = "hidden";
+    document.querySelector("#step1-arr1").style.visibility = "hidden";
+    document.querySelector("#step1-gif1").style.visibility = "visible"
 
-function step1(){
-    document.querySelector("#mainStep").style.visibility = "hidden";
-    document.querySelector("#png1-mainStep").style.visibility = "hidden";
-    document.querySelector("#next-to-step1").style.visibility = "hidden";
+    myTimeout = setTimeout(timeDelay1Step1, 1500);
+}
 
-    document.querySelector("#step1").style.visibility = "visible";
-    document.querySelector("#png1-step1").style.visibility = "visible";
-    document.querySelector("#png2-step1").style.visibility = "visible";
+// TIME DELAY FUNCTION OF STEP 1 
+
+function timeDelay1Step1(){
+}
+
+function nextToStep3(){
+    document.querySelector("#mainPage").style.visibility = "hidden"
+    document.querySelector("#step3").style.visibility = "visible";
     
-    myTimeout = setTimeout(myGreeting1Step1, 1000);
-}
- 
-function moveSheet(){
-    document.querySelector("#png1-step1").style.visibility = "hidden";
-    document.querySelector("#png2-step1").style.visibility = "hidden";
-    document.querySelector("#arr1-step1").style.visibility = "hidden";
-    document.querySelector("#trans1-step1").style.visibility = "hidden";
-
-    document.querySelector("#gif1-step1").style.visibility = "visible";
-}
-
-// TIME DELAY FUNCTION OF STEP 1
-
-function myGreeting1Step1(){
-    document.querySelector("#arr1-step1").style.visibility = "visible";
-    document.querySelector("#trans1-step1").style.visibility = "visible";
-
-    myStopFuntion();
 }
--- experiment/simulation/scripts/main.js
+++
@@ -1,7 +0,0 @@
-function indexPage(){
-    document.querySelector("#mainStep").style.visibility = "hidden";
-    document.querySelector("#next-to-index").style.visibility = "hidden";
-
-    document.querySelector("#indexPage").style.visibility = "visible";
-
-}
