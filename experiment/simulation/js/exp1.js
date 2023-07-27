bc1beb57f2710848af8652c7623e059c46dfaf2b
Varun Pal Singh <singhvarun928@gmail.com>
2023-07-27 17:10:29 +0530

    completed till exp 1 2h-pencil selection

--- experiment/simulation/js/exp1.js
+++ experiment/simulation/js/exp1.js
@@ -1,35 +1,9 @@
 // THIS SINGLE FUNCTION WILL STOP THE setTimeOut Functions
 
-function myStopFuntion(){
-    clearTimeout(myTimeout);
-}
+// function myStopFuntion(){
+//     clearTimeout(myTimeout);
+// }
 
-function nextToStarterPage(){
-    document.querySelector("#mainPage").style.visibility = "hidden";
-    document.querySelector("#step1").style.visibility = "visible";
+// function showDropMenu1{
 
-    URL("#starter.html");
-}
-
-function step1MoveSheet1(){
-    document.querySelector("#step1-png1").style.visibility = "hidden";
-    document.querySelector("#step1-trans1").style.visibility = "hidden";
-    document.querySelector("#step1-arr1").style.visibility = "hidden";
-
-    document.querySelector("#step1-gif1").style.visibility = "visible"
-
-    myTimeout = setTimeout(timeDelay1Step1, 2500);
-}
-
-// TIME DELAY FUNCTION OF STEP 1 
-
-function timeDelay1Step1(){
-    document.querySelector("#step1-gif1").style.visibility = "hidden"
-
-}
-
-function nextToStep3(){
-    document.querySelector("#mainPage").style.visibility = "hidden"
-    document.querySelector("#step3").style.visibility = "visible";
-    
-}
+// }
